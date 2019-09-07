6d041241c7f739d7ab340eeb6ef3bcbb4ce3e84b
田 <tian@tiandeMacBook-Pro-2.local>
2019-09-07 15:36:02 -0700

    finish task 3

---
+++ task3/plot.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python
+
+import csv
+import matplotlib.mlab as mlab
+import matplotlib.pyplot as plt
+import datetime
+import numpy as np
+
+def get_sec(time_str):
+	h, m, s = time_str.split(':')
+	return int(h) * 3600 + int(m) * 60 + int(s)
+
+column = []
+i = 0
+with open('intra-measurement time.txt') as csv_file:
+	csv_reader = csv.reader(csv_file, delimiter=',')
+	line_count = 0
+	station = '-1'
+	prev_time = 0;
+	for row in csv_reader:
+		num = get_sec(row[1][1:-1])
+		if num > 260 and num < 300:
+			i += 1
+		column.append(num)
+
+print(column)
+column = np.clip(column, 0, 260)
+plt.hist(column, bins=[0,100,120,130,140,150,160,170,180,190,200,220,240,260,280])
+plt.show()
---
+++ task3/process.py
@@ -0,0 +1,40 @@
+#!/usr/bin/python
+
+import csv
+import datetime
+
+#f= open("res.txt","w+")
+#with open('20190904_station_sequence.csv') as csv_file:
+#	csv_reader = csv.reader(csv_file, delimiter=',')
+#	line_count = 0
+#	station = '-1'
+#	prev_time = 0;
+#	for row in csv_reader:
+#		if line_count != 0:
+#			if station != '-1' and row[4] != station:
+#				date_time_obj = datetime.datetime.strptime(row[2], '%Y-%m-%d %H:%M:%S')
+#				diff = date_time_obj - prev_time
+#				f.write('<'+station+'>,<'+row[4]+'>,<'+str(diff)+'>\n')
+#			station = row[4]
+#			prev_time = datetime.datetime.strptime(row[2], '%Y-%m-%d %H:%M:%S')
+#			print(prev_time)
+#		line_count += 1
+#	print('Processed lines.')
+
+f= open("res2.txt","w+")
+with open('20190904_station_sequence.csv') as csv_file:
+	csv_reader = csv.reader(csv_file, delimiter=',')
+	line_count = 0
+	station = '-1'
+	prev_time = 0;
+	for row in csv_reader:
+		if line_count != 0:
+			if station != '-1' and row[4] == station:
+				date_time_obj = datetime.datetime.strptime(row[2], '%Y-%m-%d %H:%M:%S')
+				diff = date_time_obj - prev_time
+				f.write('<'+station+'>,<'+str(diff)+'>\n')
+			station = row[4]
+			prev_time = datetime.datetime.strptime(row[2], '%Y-%m-%d %H:%M:%S')
+			print(prev_time)
+		line_count += 1
+	print('Processed lines.')
