f56b6c8ebd4af35954bccd3299175aba0d7dd86e
Jasper Hsieh <jasperhsieh666@gmail.com>
2019-10-16 17:43:34 -0700

    Test Dijkstra for small graph

--- src/shortest_path.py
+++ src/shortest_path.py
@@ -8,11 +8,15 @@ def main():
 
     #initialize()
 
-    internal_get_spt_from_stat_name()
+    #internal_get_spt_from_stat_name()
 
     #graph_dic, graph_edges = get_test_graph()
     #dijkstra(graph_dic, "0", graph_edges)
     #print(graph_dic)
+
+    test_graph_adj_dic = get_test_graph_dic()
+    test_graph_edge_dic = get_test_graph_edge_dic()
+    internal_test_graph()
     pass
 
 def initialize():
@@ -66,6 +70,47 @@ def get_shortest_path_dist_from_stat(stat1, stat2, sp_dic):
 
     return sp_dic[key]['distance'], sp_dic['path']
 '''
+def internal_test_graph():
+    test_graph_adj_dic = get_test_graph_dic()
+    test_graph_edge_dic = get_test_graph_edge_dic()
+    #print(test_graph_adj_dic)
+    #print(test_graph_edge_dic)
+
+    #print(test_graph_adj_dic['0']['adj'])
+
+    internal_dijkstra('0', test_graph_adj_dic, test_graph_edge_dic)
+    #print(test_graph_adj_dic)
+
+    get_shortest_path('0', '4', test_graph_adj_dic)
+
+def internal_dijkstra(src, graph_dic, dist_dic):
+    if src not in graph_dic:
+        print("src " + str(src) + " not in roads network")
+        return
+    vertices = set(graph_dic.keys())
+    for vertex_id in graph_dic:
+        #print(vertex_id)
+        graph_dic[vertex_id]['dist'] = numpy.Inf
+        graph_dic.get(vertex_id)['prev'] = -1
+    graph_dic.get(src)['dist'] = 0
+
+    #print(graph_dic)
+    while vertices:
+        min_dist_vertex, min_dist = get_min_dist(graph_dic, vertices)
+        print('*** ', min_dist_vertex,':',  min_dist, '***')
+        vertices.remove(min_dist_vertex)
+
+        for neighbor in graph_dic.get(min_dist_vertex)['adj']:
+            if neighbor in vertices:
+                old_dist = graph_dic.get(neighbor)['dist']
+                new_dist = (graph_dic.get(min_dist_vertex)['dist'] +
+                    dist_dic[min_dist_vertex + '#' + neighbor])
+                #print('old_dist', old_dist, 'new dist',new_dist)
+                if new_dist < old_dist:
+                    #print('update', neighbor)
+                    graph_dic.get(neighbor)['dist'] = new_dist
+                    graph_dic.get(neighbor)['prev'] = min_dist_vertex
+
 def internal_get_spt_from_stat_name():
     station1 = 'CSE1'
     station2 = 'RE4'
@@ -73,24 +118,7 @@ def internal_get_spt_from_stat_name():
     graph_dic = gutil.get_graph(files.roads_pads_network_utm_geojson,
             files.roads_correction_utm_csv)
     #print(graph_dic)
-    with open('output.csv', 'w') as csv_file:
-        writer = csv.writer(csv_file)
-        for key, value in graph_dic.items():
-            writer.writerow([key, value])
-    with open('output.csv') as csv_file:
-        reader = csv.reader(csv_file)
-        mydict = dict(reader)
-        #print(mydict)
-    '''
-    #print(graph_dic)
-    with open('output.txt', 'w') as f:
-        print(graph_dic, file=f)
-        f.write(str(graph_dic))
-    with open('output.txt', 'r') as f:
-        data = f.read()
-        tmp_dic = eval(data)
-        print('tmp_dic', tmp_dic[4])
-    '''
+
     stat_id_dic = gutil.create_station_status_dic(files.station_status_utm_geojson,
             files.closest_to_road_geojson_utm )
     road_not_found_stat = add_station_to_road_mapping(stat_id_dic, graph_dic)
@@ -152,18 +180,19 @@ def get_shortest_path(src, dst, graph_dic):
     dijkstra(src, graph_dic)
     path = get_dijkstra_path(src, dst, graph_dic)
     dist = get_dist_from_path(path, graph_dic)
-    #print(path, dist)
+    print(path, dist)
     return path, dist
-
+'''
 def get_dist_from_path(path, graph_dic):
     total = 0
     for vertex in path:
         if vertex not in graph_dic:
+            print('path vertex not in graph_dic')
             return -1
         else:
             total += graph_dic.get(vertex)['dist']
     return total
-
+'''
 def get_dijkstra_path(src, dst, graph_dic):
     if src  not in graph_dic or dst not in graph_dic:
         print(src, 'or', dst, 'not found in graph_dic')
@@ -214,30 +243,27 @@ def get_min_dist(graph_dic, vertex_set):
             min_dist = graph_dic.get(vertex)['dist']
     return min_dist_vertex, min_dist
 
-def get_test_graph():
-    graph_dic = {}
-    graph_edges = {}
-    
-    with open(files.tmp_adj_json) as json_file:
-        graph_dic = json.load(json_file)
-    
-    with open(files.tmp_dist_json) as json_file:
-        graph_edges = json.load(json_file)
+def get_test_graph_dic():
+    with open(files.test_graph_adj_csv) as csv_file:
+        reader = csv.reader(csv_file)
+        mydict = dict(reader)
+        #print(mydict)
+    for vertex in mydict:
+        obj = eval(mydict[vertex])
+        mydict[vertex] = obj
+    return mydict
 
-    #print(graph_dic)
-    #print(graph_edges)
-    return graph_dic, graph_edges
-
-def get_test_dst(v1, v2, graph_edges):
-    if v1 > v2:
-        tmp = v1
-        v1 = v2
-        v2 = tmp
-    key = str(v1) + '#' + str(v2)
-    if key in graph_edges:
-        return graph_edges.get(key)
-    print("edge:" + key + " not found")
-
-
+def get_test_graph_edge_dic():
+    test_graph_edge_dic = {}
+    with open(files.test_graph_edge_csv) as f:
+        reader = csv.DictReader(f, delimiter=',')
+        for row in reader:
+            #print(row)
+            key1 = row['vertex1'] + '#' + row['vertex2']
+            key2 = row['vertex2'] + '#' + row['vertex1']
+            test_graph_edge_dic[key1] = int(row['edge'])
+            test_graph_edge_dic[key2] = int(row['edge'])
+    #print(test_graph_edge_dic)
+    return test_graph_edge_dic
 if __name__ == '__main__':
     main()
