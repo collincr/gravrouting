e4e0f53b38a0f74896b920d50683fa8ab14c7836
田 <tian@tiandeMacBook-Pro-5.local>
2019-11-25 18:02:07 -0800

    change dynamic algorithm

--- src/dynamic_routing.py
+++ src/dynamic_routing.py
@@ -86,75 +86,119 @@ def add_visit_timestamp(stations):
 	return departure_time, res
 
 def begin_routing(stations_everyday):
-	day = input("Input your current day: ")
-	begin_time = input("Input your begin time: ")
-	being_time = get_sec(begin_time)
-	clusters = stations_everyday[day]
-	last_time_repeat = 0
-	current_station_index = 0
-	current_cluster_index = 0
-	stations = clusters[current_cluster_index]
-	visit_path = []
-	visit_time = []
-	visit_current_time = []
-
-	while(True):
-		station_name = input("Input your current station name: ")
-		current_time = input("Input your current time: ")
-		
-		visit_current_time.append(current_time)
-		current_time = get_sec(current_time)
-		
-		current_time = current_time - being_time
-		visit_time.append(current_time)
-		
-		if station_name in visit_path:
-			last_time_repeat = current_time
-		
-		visit_path.append(station_name)
-		target = stations[0]
-		if station_name in stations: # If on the right station
-			stations.remove(station_name)
-			if current_time - last_time_repeat > N:
-				if len(visit_path) != 0:
-					# Get travel time to visited stations
-					station_travel_time = {}
-					for i in range(0, len(visit_path)):
-						if station_name == visit_path[i]:
-							continue
-						travel_time = getTravelTime(station_name, visit_path[i])
-
-						if current_time - visit_time[i] + travel_time > N and travel_time < M:
-							station_travel_time[visit_path[i]] = travel_time
-					
-					# Choose the station with minimum travel time
-					if len(station_travel_time) > 0:
-						sorted_s = sorted(station_travel_time.items(), key=operator.itemgetter(1))
-						sorted_station = collections.OrderedDict(sorted_s)
-						first_station = next(iter(sorted_station))
-						
-						# Update the new order
-						new_list = []
-						new_list.append(first_station)
-						for station in stations:
-							new_list.append(station)
-						stations = get_permutation_start_with_station(new_list, first_station)
-					else:
-						print("Timt to visit repeat station, but no satisified stations!")
-			
-			elif station_name != target:
-				# Update the new order
-				stations = get_permutation_with_mini_time(stations)
+	print('Initial path:')
+	for day in stations_everyday:
+		each_day = stations_everyday[day]
+		dat_str = []
+		for clusters in each_day:
+			dat_str = dat_str + clusters
+		print('day' + day + ' path: ' + str(dat_str))
 				
-			if len(stations) == 0:
-				current_cluster_index += 1
-				if current_cluster_index >= len(clusters):
-					print("You have visited all stations, good job!")
-					return
-				stations = clusters[current_cluster_index]
+	while(True):
+		day = input("Input your current day: ")
+		begin_time = input("Input your begin time: ")
+		being_time = get_sec(begin_time)
+		clusters = stations_everyday[day]
+		del stations_everyday[day]
+		last_time_repeat = 0
+		current_station_index = 0
+		current_cluster_index = 0
+		stations = clusters[current_cluster_index]
+		visit_path = []
+		visit_time = []
+		visit_current_time = []
+		Need_to_repeat = None
 
-		print("Next station is: " + stations[0])
+		while(True):
+			station_name = input("Input your current station name: ")
+			current_time = input("Input your current time: ")
+			
+			visit_current_time.append(current_time)
+			current_time = get_sec(current_time)
+			
+			current_time = current_time - being_time
+			visit_time.append(current_time)
+			
+			if station_name in visit_path:
+				last_time_repeat = current_time
+			
+			visit_path.append(station_name)
+			target = stations[0]
+			if station_name in stations: # If on the right station
+				if Need_to_repeat != None:
+					stations.remove(Need_to_repeat)
+					Need_to_repeat = None
+				try:
+					stations.remove(station_name)
+				except ValueError:
+					pass
+				if current_time - last_time_repeat > N:
+					if len(visit_path) != 0:
+						# Get travel time to visited stations
+						station_travel_time = {}
+						for i in range(0, len(visit_path)):
+							if station_name == visit_path[i]:
+								continue
+							travel_time = getTravelTime(station_name, visit_path[i])
 
+							if current_time - visit_time[i] + travel_time > N and travel_time < M:
+								station_travel_time[visit_path[i]] = travel_time
+						
+						# Choose the station with minimum travel time
+						if len(station_travel_time) > 0:
+							sorted_s = sorted(station_travel_time.items(), key=operator.itemgetter(1))
+							sorted_station = collections.OrderedDict(sorted_s)
+							first_station = next(iter(sorted_station))
+							Need_to_repeat = first_station
+							# Update the new order
+							new_list = []
+							new_list.append(first_station)
+							for station in stations:
+								new_list.append(station)
+							stations = get_permutation_start_with_station(new_list, first_station)
+						else:
+							print("Timt to visit repeat station, but no satisified stations!")
+				
+				elif station_name != target:
+					# Update the new order
+					stations = get_permutation_with_mini_time(stations)
+					
+				if len(stations) == 0:
+					del clusters[current_cluster_index]
+					current_cluster_index += 1
+					if current_cluster_index >= len(clusters):
+						print("You have visited all stations for today, good job!")
+						break
+					stations = clusters[current_cluster_index]
+
+			travel_time = getTravelTime(station_name, stations[0])
+			back_time = getTravelTime(stations[0], 'CS25')
+			if current_time + travel_time + back_time > 3600 * 8:
+				if Need_to_repeat != None:
+					try:
+						stations.remove(Need_to_repeat)
+					except ValueError:
+						pass
+				# Cut the current cluster
+				left_clusters = {}
+				id = 1
+				for cluster in clusters:
+					left_clusters[str(id)] = {}
+					left_clusters[str(id)]['visited'] = False
+					left_clusters[str(id)]['stations'] = cluster
+					id += 1
+				for left_day in stations_everyday:
+					for cluster in stations_everyday[left_day]:
+						left_clusters[str(id)] = {}
+						left_clusters[str(id)]['visited'] = False
+						left_clusters[str(id)]['stations'] = cluster
+						id += 1
+			
+			print("Next station is: " + stations[0])
+		
+		if len(stations_everyday) == 0:
+			print("You have visited all stations, good job!")
+			return
 
 
 if __name__ == '__main__':
